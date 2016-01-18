21afa1bd4f027c74bafaeffc166cff7f2e9608e9
Aris Winardi <me@arwin.my.id>
2016-01-18 15:48:22 +0700

    simpan data berhasil

--- android/uangku/app/src/main/java/id/my/arwin/uangku/tambahdata_activity.java
+++ android/uangku/app/src/main/java/id/my/arwin/uangku/tambahdata_activity.java
@@ -2,8 +2,11 @@ package id.my.arwin.uangku;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -14,13 +17,23 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 
 /**
  * Created by winardiaris on 1/15/16.
  */
 public class tambahdata_activity extends FragmentActivity {
     String type = "in";
+    private static final String opl = "newdata";
+    private static final String TAG_STATUS = "status";
+    sessiomanager session;
 
 
 
@@ -28,6 +41,8 @@ public class tambahdata_activity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tambahdata_activity);
+        //session
+        session = new sessiomanager(getApplicationContext());
 
         final EditText tdate = (EditText) findViewById(R.id.Tdate);
         final EditText tanggal_ = (EditText) findViewById(R.id.Tdate);
@@ -60,12 +75,15 @@ public class tambahdata_activity extends FragmentActivity {
         bsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String op = opl.toString();
                 String jenis = type;
-
                 final String tanggal = tanggal_.getText().toString();
-                String jumlah = jumlah_.getText().toString();
-                String bukti = bukti_.getText().toString();
-                String ket = ket_.getText().toString();
+                final String jumlah = jumlah_.getText().toString();
+                final String bukti = bukti_.getText().toString();
+                final String ket = ket_.getText().toString();
+
+                HashMap<String, String> user = session.getUserDetails();
+                String uid = user.get(sessiomanager.TAG_UID);// get uid
 
                 if(tanggal.equals("")){
                     Toast.makeText(tambahdata_activity.this, "Isi tanggal terlebih dahulu", Toast.LENGTH_SHORT).show();
@@ -91,28 +109,8 @@ public class tambahdata_activity extends FragmentActivity {
                     Log.d("bukti", bukti);
                     Log.d("ket", ket);
 
+                    new simpandata(tambahdata_activity.this).execute(op,uid,tanggal,bukti,jenis,jumlah,ket);
 
-                    Toast.makeText(tambahdata_activity.this, "Data berhasil disimpan", Toast.LENGTH_SHORT).show();
-                    new AlertDialog.Builder(tambahdata_activity.this).setIcon(android.R.drawable.ic_dialog_info).setTitle("Info")
-                            .setMessage("Data berhasil disimpan,ingin tambah baru?")
-                            .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-
-                                    Intent i = new Intent(tambahdata_activity.this,menuutama_activity.class);//nanti ganti ke data
-                                    startActivity(i);
-                                    finish();
-                                }
-                            }).setPositiveButton("Ya", new DialogInterface.OnClickListener(){
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    tanggal_.setText("");
-                                    jumlah_.setText("");
-                                    bukti_.setText("");
-                                    ket_.setText("");
-                                    tanggal_.requestFocus();
-                                }
-                    }).show();
                 }
             }
         });
@@ -134,5 +132,93 @@ public class tambahdata_activity extends FragmentActivity {
                     break;
         }
         Log.d("radio type:",type);
+    }
+
+    private class simpandata extends AsyncTask<String, Void, String> {
+        ProgressDialog dialog;
+        private Context context1;
+
+        public simpandata(Context context) {
+            this.context1 = context;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(context1);
+            dialog.setMessage("Tunggu sebentar ya..");
+            dialog.show();
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            String result = null;
+            ArrayList<NameValuePair> post_parameter = new ArrayList<>();
+            post_parameter.add(new BasicNameValuePair("op", params[0]));
+            post_parameter.add(new BasicNameValuePair("uid", params[1]));
+            post_parameter.add(new BasicNameValuePair("date", params[2]));
+            post_parameter.add(new BasicNameValuePair("token", params[3]));
+            post_parameter.add(new BasicNameValuePair("type", params[4]));
+            post_parameter.add(new BasicNameValuePair("value", params[5]));
+            post_parameter.add(new BasicNameValuePair("desc", params[6]));
+            try{
+                String jsonStr = CustomHTTPClient.executeHttpPost("http://192.168.1.22/uangku1.0.1/", post_parameter);
+                if (jsonStr != null) {
+                    try {
+                        JSONObject jsonObj = new JSONObject(jsonStr);
+                        String status = jsonObj.getString(TAG_STATUS);
+                        result = status;
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    Log.e("ServiceHandler", "Couldn't get any data from the url");
+                }
+
+            }catch (Exception e){
+                result = e.toString();
+            }
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            dialog.dismiss();
+            super.onPostExecute(s);
+//            finish();
+            //jika berhasil menyimpan
+            if(s=="1"){
+                Toast.makeText(tambahdata_activity.this, "Data berhasil disimpan", Toast.LENGTH_SHORT).show();
+                new AlertDialog.Builder(tambahdata_activity.this).setIcon(android.R.drawable.ic_dialog_info).setTitle("Info")
+                        .setMessage("Data berhasil disimpan,ingin tambah baru?")
+                        .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                Intent i = new Intent(tambahdata_activity.this,menuutama_activity.class);//nanti ganti ke data
+                                startActivity(i);
+                                finish();
+                            }
+                        }).setPositiveButton("Ya", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText tanggal_ = (EditText) findViewById(R.id.Tdate);
+                        EditText jumlah_ = (EditText) findViewById(R.id.tvalue);
+                        EditText bukti_ = (EditText) findViewById(R.id.ttoken);
+                        EditText ket_ = (EditText) findViewById(R.id.tdesc);
+
+                        tanggal_.setText("");
+                        jumlah_.setText("");
+                        bukti_.setText("");
+                        ket_.setText("");
+                        tanggal_.requestFocus();
+                    }
+                }).show();
+            }
+            else if(s=="2"){
+                Toast.makeText(tambahdata_activity.this, "??", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(tambahdata_activity.this,"Gagal menyimpan",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
